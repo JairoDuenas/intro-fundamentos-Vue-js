1c069fa71433e3c4ff8f41f4a9b29251a8ac81d4
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-08 14:54:49 -0500

    Vue progresivo

---
+++ Hola.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Lorem ipsum dolor sit</p>
+    <div>{{ text }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Hola!",
+    };
+  },
+};
+</script>
+
+<style>
+p {
+  color: brown;
+}
+</style>
